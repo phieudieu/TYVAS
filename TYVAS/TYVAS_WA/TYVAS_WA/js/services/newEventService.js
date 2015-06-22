75ba0c2662dc5fba69ab3f9a7b543ccc4c5f5b07
phieudieu <phieudieu54@gmail.com>
2015-06-21 22:56:16 -0500

    [Nuong] fix bug field name

--- TYVAS/TYVAS_WA/TYVAS_WA/js/services/newEventService.js
+++ TYVAS/TYVAS_WA/TYVAS_WA/js/services/newEventService.js
@@ -13,26 +13,26 @@ define(['js/common/service'],function(mod){
     NewEventService.prototype.newEvent = function(){
         var self = this;
         self.event = {
-            m_ID : null,
-            m_Title : Null,
-            m_Description : Null,
-            m_Address : Null,
-            m_Reference : Null,
-            m_StartDate: null,
-            m_EndDate: null,
-            m_Images : Null,
-            m_Banner : Null,
-            m_Status : null,
-            m_Amount : null,
-            m_FeeJoin : null,
-            m_NUMBER : null
+            ID : null,
+            Title : Null,
+            Description : Null,
+            Address : Null,
+            Reference : Null,
+            StartDate: null,
+            EndDate: null,
+            Images : Null,
+            Banner : Null,
+            Status : null,
+            Amount : null,
+            FeeJoin : null,
+            NUMBER : null
         }
         this.listRef = [];
     }
     NewEventService.prototype.addNewEvent = function() {
         var self =this;
         var url = "api/Events";
-        self.event.m_Reference = self.listRef;
+        self.event.Reference = self.listRef;
         if(self.event.m_ID != null) {
             url="api/Events/fff";
         }
