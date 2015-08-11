027118ac26b255e4a0ff83db4f88b2c874f5f18d
phieudieu <phieudieu54@gmail.com>
2015-08-10 20:53:35 -0500

    commit mainPage

---
+++ TYVAS/TYVAS_WA/TYVAS_WA/MainPage.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainPage.aspx.cs" Inherits="TYVAS_WA.MainPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <p>I am main page</p>
+    </div>
+    </form>
+</body>
+</html>
---
+++ TYVAS/TYVAS_WA/TYVAS_WA/MainPage.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TYVAS_WA
+{
+    public partial class MainPage : System.Web.UI.Page
+    {
+        public class Global : System.Web.HttpApplication
+        {
+            protected void Application_BeginRequest(object sender, EventArgs e)
+            {
+                if (Request.Url.AbsolutePath.EndsWith("/"))
+                {
+                    Server.Transfer(Request.Url.AbsolutePath + "index.aspx");
+                }
+            }
+        }
+    }
+}
